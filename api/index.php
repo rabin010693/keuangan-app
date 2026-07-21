9c607035450713ce344b626979dcd489ebb5e01a
rabin010693 <rabinsyn874@gmail.com>
2026-07-21 23:07:40 +0700

    Force display PHP errors for debugging

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
+// Paksa tampilkan semua error PHP ke browser
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 chdir(__DIR__ . '/..');
 require __DIR__ . '/../index.php';
